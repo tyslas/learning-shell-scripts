ef1194149074fb1b708aa388eedb5c1a41a1f4ea
Tito Yslas <roberto.yslas@gmail.com>
2018-05-09 15:56:47 -0600

    learning about 'test' or '[': how to use it, when to use it, and some conventions

---
+++ magicNum.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo -en "please guess the magic number: "
+read X
+echo $X | grep "[^0-9]" > /dev/null 2>&1
+if [ "$?" -eq "0" ]; then
+  # if the grep found something other than 0-9
+  # then it's not an integer
+  echo "sorry, please enter a number"
+else
+  # the grep found only 0-9, so it's an integer
+  # we can safely do a test on it
+  if [ "$X" -eq "7" ]; then
+    echo "you entered the magic number!"
+  fi
+fi
---
+++ test2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+X=0
+while [ -n "$X" ]
+do
+  echo "enter some text (RETURN to quit)"
+  read X
+  if [ -n "$X"  ]; then
+    echo "you said: $X"
+  fi
+done
