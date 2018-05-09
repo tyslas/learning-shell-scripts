09f1ca81ac69d1038e6caebc68d2aa4fc250bd31
Tito Yslas <roberto.yslas@gmail.com>
2018-05-09 11:44:30 -0600

    for and while loops

---
+++ while.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+INPUT_STRING=hello
+while [ "$INPUT_STRING" != "bye" ]
+do
+  echo "please type something in (bye to quit)"
+  read INPUT_STRING
+  echo "you typed: $INPUT_STRING"
+done
---
+++ while2.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+while :
+do
+  echo "please type something & press enter (^C to quit)"
+  read INPUT_STRING
+  echo "you typed: $INPUT_STRING"
+done
